a4c7cda42ee0242b741791b4e15b69991a1aaa07
ele <eselectron@163.com>
2021-09-10 00:17:49 +0800

    LambdaTest

---
+++ src/main/java/art/bytes/invokedynamic/LambdaTest.java
@@ -0,0 +1,44 @@
+package art.bytes.invokedynamic;
+
+import java.lang.reflect.Method;
+
+public class LambdaTest {
+
+    private String insStr;
+
+    private static String staticStr;
+
+    public interface Lambda {
+         void run();
+    }
+
+    public static void doSomething2(Lambda lambda) {
+        lambda.run();
+    }
+
+    public void doSomething(Lambda lambda) {
+        lambda.run();
+    }
+
+    public void insStr() { // 闭包情况 生成的lambda方法必须为非静态方法
+        doSomething(() -> System.out.println(insStr));
+    }
+
+    public void staticStr() {
+        doSomething(() -> System.out.println(staticStr));
+    }
+
+    public void constantStr() {
+        doSomething(() -> System.out.println("a"));
+    }
+
+    public static void main(String[] args) {
+        System.out.println("LambdaTest.ClientTest.class.getDeclaredMethods()");
+        for (Method m : LambdaTest.class.getDeclaredMethods()) {
+            if (m.toString().contains("lambda$")) {
+                System.out.println("\t" + m);
+            }
+        }
+    }
+
+}
